e0c9131b86da60f2d37fd4af273b75837b5d980f
JaeBum Lee <wo9374@naver.com>
2023-12-01 02:10:24 +0900

    refactor: remote 호출 부분 Repository로 구성

---
+++ DesignPattern/app/src/main/java/com/ljb/designpattern/NewsRepository.kt
@@ -0,0 +1,23 @@
+package com.ljb.designpattern
+
+import com.ljb.designpattern.mvc.UiState
+import com.ljb.network.RetrofitService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+
+class NewsRepository{
+    suspend fun getSearchNews(keyword: String): Flow<UiState<NewsResponse>> = flow {
+        RetrofitService.naverService.getSearchNews(query = keyword, sort = "sim").run {
+            if (isSuccessful) {
+                body()?.let { response ->
+                    if (response.items.isEmpty())
+                        emit(UiState.Empty)
+                    else
+                        emit(UiState.Complete(response))
+                }
+            } else
+                emit(UiState.Fail(message()))
+        }
+    }
+}
--- DesignPattern/app/src/main/java/com/ljb/designpattern/mvc/MvcActivity.kt
+++ DesignPattern/app/src/main/java/com/ljb/designpattern/mvc/MvcActivity.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.lifecycleScope
 import com.ljb.base.BaseActivity
 import com.ljb.designpattern.NewsAdapter
 import com.ljb.designpattern.NewsDecoration
+import com.ljb.designpattern.NewsRepository
 import com.ljb.designpattern.NewsResponse
 import com.ljb.designpattern.R
 import com.ljb.designpattern.databinding.ActivityMvcBinding
@@ -27,40 +28,52 @@ class MvcActivity : BaseActivity<ActivityMvcBinding>(R.layout.activity_mvc) {
     private val newsList: StateFlow<UiState<NewsResponse>> get() = _newsList
     private val newsAdapter = NewsAdapter()
 
+    private val newsRepository = NewsRepository()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.lifecycleOwner = this
 
-        binding.searchView.setOnQueryTextListener(object : OnQueryTextListener {
+        binding.apply {
+            lifecycleOwner = this@MvcActivity
 
-            //검색을 완료 하였을 경우 (키보드 '검색' 돋보기 버튼을 선택 하였을 경우)
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return if (query.isNullOrEmpty()) {
-                    Toast.makeText(this@MvcActivity, getString(R.string.query_hint), Toast.LENGTH_SHORT).show()
-                    true    //키보드 내리지 않음
-                } else {
-                    getSearchNaverNews(query)
-                    false   //키보드 내림
+            searchView.setOnQueryTextListener(object : OnQueryTextListener {
+
+                //검색을 완료 하였을 경우 (키보드 '검색' 돋보기 버튼을 선택 하였을 경우)
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return if (query.isNullOrEmpty()) {
+                        Toast.makeText(this@MvcActivity, getString(R.string.query_hint), Toast.LENGTH_SHORT).show()
+                        true    //키보드 내리지 않음
+                    } else {
+                        lifecycleScope.launch(Dispatchers.IO){
+                            _newsList.emit(UiState.Loading)
+                            newsRepository.getSearchNews("안드로이드").collectLatest { _newsList.emit(it) }
+                        }
+                        false   //키보드 내림
+                    }
                 }
-            }
 
-            //검색어를 변경할 때마다 실행
-            override fun onQueryTextChange(newText: String?): Boolean = false
-        })
-        binding.recycler.adapter = newsAdapter
-        binding.recycler.addItemDecoration(NewsDecoration())
+                //검색어를 변경할 때마다 실행
+                override fun onQueryTextChange(newText: String?): Boolean = false
+            })
+
+            //Recycler 설정
+            recycler.adapter = newsAdapter
+            recycler.addItemDecoration(NewsDecoration())
+        }
 
         observeData()
 
-        getSearchNaverNews("안드로이드")
+        //초기 "안드로이드" 검색
+        lifecycleScope.launch(Dispatchers.IO){
+            _newsList.emit(UiState.Loading)
+            newsRepository.getSearchNews("안드로이드").collectLatest { _newsList.emit(it) }
+        }
     }
 
     private fun observeData() = lifecycleScope.launch {
-        newsList
-            .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+        newsList.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
             .collectLatest { uiState ->
-
                 when (uiState) {
                     is UiState.Complete, is UiState.Empty, is UiState.Fail -> {
                         binding.progressCircular.setVisibility(false)
@@ -78,28 +91,6 @@ class MvcActivity : BaseActivity<ActivityMvcBinding>(R.layout.activity_mvc) {
                     else -> binding.progressCircular.setVisibility(true)
                 }
             }
-    }
-
-
-    fun getSearchNaverNews(keyword: String) {
-        lifecycleScope.launch {
-            withContext(Dispatchers.IO) {
-
-                _newsList.emit(UiState.Loading)
-
-                naverService.getSearchNews(query = keyword, sort = "sim").run {
-                    if (isSuccessful) {
-                        body()?.let { response ->
-                            if (response.items.isEmpty())
-                                _newsList.emit(UiState.Empty)
-                            else
-                                _newsList.emit(UiState.Complete(response))
-                        }
-                    } else
-                        _newsList.emit(UiState.Fail(message()))
-                }
-            }
-        }
     }
 }
 
