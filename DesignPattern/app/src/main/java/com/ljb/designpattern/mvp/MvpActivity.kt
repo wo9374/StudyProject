5ac0169ec49f41e5c4281d4bb1d2cc5432a1ffaa
JaeBum Lee <wo9374@naver.com>
2023-12-02 00:50:11 +0900

    style: 주석 정리

--- DesignPattern/app/src/main/java/com/ljb/designpattern/mvp/MvpActivity.kt
+++ DesignPattern/app/src/main/java/com/ljb/designpattern/mvp/MvpActivity.kt
@@ -23,8 +23,6 @@ class MvpActivity: BaseActivity<ActivityPatternsBinding>(R.layout.activity_patte
     private val newsAdapter = NewsAdapter()
 
     private val queryTextListener = object : OnQueryTextListener {
-
-        //검색을 완료 하였을 경우 (키보드 '검색' 돋보기 버튼을 선택 하였을 경우)
         override fun onQueryTextSubmit(query: String?): Boolean {
 
             if (query != null) {
@@ -32,16 +30,11 @@ class MvpActivity: BaseActivity<ActivityPatternsBinding>(R.layout.activity_patte
                     presenter.loadData(query)
                 }
             }
-
-            //return false   //키보드 내림
-            return true     //키보드 내리지 않음
+            return true
         }
 
-        //검색어를 변경할 때마다 실행
         override fun onQueryTextChange(newText: String?): Boolean {
             return false
-            //제안을 표시하는 기본 작업을 수행해야 하는 경우 false
-            //해당 작업이 리스너에 의해 직접 처리하는 경우 true
         }
     }
 
