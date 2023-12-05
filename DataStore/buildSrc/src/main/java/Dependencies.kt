a1d972a1d4f0957cc89f6b5e189a730f161ec6e9
JaeBum Lee <wo9374@naver.com>
2023-12-06 02:12:04 +0900

    build: Kotlin DSL 적용

---
+++ DataStore/buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,8 @@
+object Versions {
+    const val dataStore = "1.0.0"
+}
+
+object Libs {
+    const val preferences = "androidx.datastore:datastore-preferences:${Versions.dataStore}"
+    const val preferencesCore = "androidx.datastore:datastore-preferences-core:${Versions.dataStore}"
+}
