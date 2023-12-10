1ff197c0ca32c7aad6eedf6a046d2c4be2fd7fd5
JaeBum Lee <wo9374@naver.com>
2023-12-10 23:31:53 +0900

    Notification Initial commit

---
+++ Notification/app/src/main/java/com/ljb/notification/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.ljb.notification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.Color
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import com.ljb.notification.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
